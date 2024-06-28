2245d217e506e32757a482b7f5a2e641adb0ae07
Calyen93 <genco.claudio3@gmail.com>
2024-06-28 14:04:12 +0200

    Solution

--- src/main/java/com/example/deplo01/controllers/DeployController.java
+++ src/main/java/com/example/deplo01/controllers/DeployController.java
@@ -1,13 +1,17 @@
 package com.example.deplo01.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DeployController {
 
-    @GetMapping("/devName")
+    @Value("${dev.name}")
+    private String devName;
+
+    @GetMapping("/devname")
     public String getDevName() {
-        return "devName";
+        return devName;
     }
 }
